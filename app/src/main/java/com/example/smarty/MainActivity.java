50cc98b02dd6a09963ba58102ea0c40241620458
mhadhbimouwahed <mhadhbi.mouwahed@ENICAR.u-carthage.tn>
2021-07-14 20:13:05 +0100

    nothing much

--- app/src/main/java/com/example/smarty/MainActivity.java
+++ app/src/main/java/com/example/smarty/MainActivity.java
@@ -1,26 +1,33 @@
 package com.example.smarty;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,8 +36,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView login;
     private TextView forgotPassword;
     private TextView signup;
-    private TextView skip;
+
     private FirebaseAuth firebaseAuth;
+
+    private static String TAG="FAILURE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +47,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         firebaseAuth=FirebaseAuth.getInstance();
 
+
         email=findViewById(R.id.email);
         password=findViewById(R.id.password);
         login=findViewById(R.id.login);
         forgotPassword=findViewById(R.id.forgotPassword);
         signup=findViewById(R.id.signup);
-        skip=findViewById(R.id.skip);
+
+
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,10 +73,14 @@ public class MainActivity extends AppCompatActivity {
                     }else{
                         Login();
                     }
-
                 }
+            }
+        });
 
-
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog();
             }
         });
 
@@ -86,28 +102,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        skip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),NotLoggedinActivity.class));
-            }
-        });
+
 
 
 
     }
+
+
+
     @Override
     protected void onStart(){
         super.onStart();
+
+
+
         FirebaseUser user=firebaseAuth.getCurrentUser();
         if(user!=null){
             startActivity(new Intent(getApplicationContext(),MainPage.class));
         }
     }
-
-
-
-
 
     public void Login(){
 
@@ -125,6 +138,35 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                     });
+
+    }
+
+    private void createDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Facebook Login");
+        builder.setMessage("you can login with facebook instead");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Toast.makeText(getApplicationContext(),"successsssss",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        builder.show();
+    }
+
+    private void updateUI(FirebaseUser user){
+        if (user!=null){
+            Toast.makeText(getApplicationContext(),"welcome "+user.getDisplayName(),Toast.LENGTH_LONG);
+        }
     }
 
 }
